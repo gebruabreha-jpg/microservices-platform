eddbc08cab9229a89b8e732ddd83682f84540d1f
Gebru Abreha Woldeyohannes <gebru.abreha.woldeyohannes@ericsson.com>
2026-08-10 16:22:27 +0200

    main.py

--- services/order-service/app/main.py
+++ services/order-service/app/main.py
@@ -3,7 +3,7 @@ from app.routes import router as order-router
 
 #FastAPI app + router includes
 app = FastAPI(title="order-service")
-app.include_router(router)
+app.include_router(order-router)
 
 @app.get("/")
 async def root():
