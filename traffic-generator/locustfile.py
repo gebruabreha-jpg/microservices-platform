6095e992c8ac732122f58fa0796d0595395c97f5
Gebru Abreha <gebru.abreha@alumni.ie.edu>
2026-08-24 23:47:10 +0200

    tmp

--- traffic-generator/locustfile.py
+++ traffic-generator/locustfile.py
@@ -28,7 +28,7 @@ class OrderUser(FastHttpUser):
             "amount": round(random.uniform(5.0, 200.0), 2),
         }
          # self.client.rest automatically catches non-2xx status codes as failures instade of:- with self.client.post(
-          with self.client.rest("POST", "/orders", json=payload, name="/orders") as response:
+        with self.client.rest("POST", "/orders", json=payload, name="/orders") as response:
             if response.status_code == 200 and "id" in response.js:
                 self.created_order_ids.append(response.js["id"])
 
